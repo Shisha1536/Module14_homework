2b24d21c07dbab7dacbb9a73c913713db2eb24da
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2023-09-09 15:39:33 +0300

    task_5 fin

--- task_5/task_5.js
+++ task_5/task_5.js
@@ -1,15 +1,33 @@
 const btn = document.querySelector('.btn');
 let div = document.querySelector('div');
+let myJson = localStorage.getItem('myJson');
+if (myJson) {
+    let data = JSON.parse(myJson)
+    let cards = '';
+    data.forEach(item => {
+    const cardBlock = `
+        <div class="card">
+        <img
+            src="${item.url}"
+            class="card-image"
+        />
+        <p>${item.title}</p>
+        </div>
+    `;
+    cards = cards + cardBlock;
+    });
+    div.innerHTML = cards;
+}
 btn.addEventListener('click', () => {
     const page = Number(document.querySelector('.page').value);
     const limit = Number(document.querySelector('.limit').value);
-    debugger
     if (page >= 1 && page <= 10 && limit >= 1 && limit <= 10) {
         fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
+                localStorage.setItem("myJson", JSON.stringify(data));
                 let cards = '';
                 data.forEach(item => {
                 const cardBlock = `
